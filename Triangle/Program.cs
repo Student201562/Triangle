c9c81fbf0d3b62843b0c76dfc1f69facadc4aa86
student201562 <Kirill_kov81@mail.ru>
2017-02-15 21:00:56 +0500

    Добавлены комментарии к каждому методу

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -12,11 +12,19 @@ namespace Triangle
         static double[] sumTwoPoint;
         static void Main(string[] args)
         {
+            // Баловался 
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.Write("\r {0} %", i);
+                System.Threading.Thread.Sleep(10);
+            }
+            Console.WriteLine();
+
             Random gen = new Random();
             Point[] points = new Point[3];
             Edge[] edges = new Edge[3];
 
-            Console.WriteLine("Введите количесто треугольников = ");
+            Console.Write("Введите количесто треугольников = ");
             Triangle[] triangle = new Triangle[Convert.ToInt32(Console.ReadLine())];
             // Заполнение точек
             for (int i = 0; i < triangle.Length; i++)
@@ -27,8 +35,8 @@ namespace Triangle
                 }
                 triangle[i] = new Triangle(points);
             }
+            // Вывожу один раз в качестве проверке
             PrintPoints(points);
-
             // Заполнение ребер
             for (int i = 0; i < edges.Length; i++)
             {
@@ -37,33 +45,46 @@ namespace Triangle
                 else
                     edges[i] = new Edge(points[i], points[i + 1]);
             }
+            // Формирование ребер и нахождение длины ребер, не обязательный метод, 
+            // так как уже есть похожий FormedLength
+            ShapeEdges(edges);
 
-                // Формирование ребер и нахождение длины ребер
-                double[] findEdge = ShapeEdges(edges, points);
+            for (int i = 0; i < triangle.Length; i++)
+            {
+                double[] findEdge = FormedLength(triangle[i]);
                 // Нахождение периметра
-                double perimetrTriangle = Perimeter(triangle[i], findEdge);
+                double perimetrTriangle = Perimeter(findEdge);
                 // Нахождение площади
-                double areaTriangle = Area(triangle, findEdge, perimetrTriangle);
+                double areaTriangle = Area(findEdge, perimetrTriangle);
                 // Нахождение прямоугольного треугольника
-                RightTriangle(triangle, findEdge);
+                RightTriangle(findEdge);
                 // Нахождение равнобедренного треугольника
-                IsoscelesTriangle(triangle, findEdge);
-            
+                IsoscelesTriangle(findEdge);
+                Console.WriteLine(@"
+       ………(\__/)
+      ………(=’.’=) -- Улыбнулся, это {0} треугольник
+ ..…є[:]||||||||[:]э
+      ………(`)_(`)", i);
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
+        // Метод вывода точек на консоль
         static void PrintPoints(Point[] points)
         {
+            Console.WriteLine("\nТочки!");
             for(int i = 0; i < points.Length; i++)
             {
                 Console.Write(points[i].x + " " + points[i].y);
                 Console.WriteLine();
             }
         }
-        static double[] ShapeEdges(Edge[] edges, Point[] points)
+        // Метод формирования ребер, проблема этого метода, он считает,
+        // для одного триугольника, а метод FormedLength - считает для всех
+        static void ShapeEdges(Edge[] edges)
         {
             sumTwoPoint = new double[3];
-
-            double[] edgesLength = new double[3];
+            //double[] edgesLength = new double[3];
             Console.WriteLine("Значение ребр");
             for (int i = 0; i < edges.Length; i++)
             {      
@@ -71,34 +92,55 @@ namespace Triangle
                 {
                     sumTwoPoint[i] = Math.Sqrt(Math.Pow((edges[i-2].b.x - edges[i].a.x),2) +
                         Math.Pow((edges[i-2].b.y - edges[i].a.y), 2));
-
-                                                                    edgesLength[i] = sumTwoPoint[i];
+                                                                   // edgesLength[i] = sumTwoPoint[i];
                     Console.WriteLine(sumTwoPoint[i]);
                 }
                 else
                 {
                     sumTwoPoint[i] = Math.Sqrt(Math.Pow((edges[i+1].b.x - edges[i].a.x), 2) +
                         Math.Pow((edges[i+1].b.y - edges[i].a.y), 2));
-
-                                                                    edgesLength[i] = sumTwoPoint[i];
+                                                                    //edgesLength[i] = sumTwoPoint[i];
                     Console.WriteLine(sumTwoPoint[i]);
                 }
             }
-            return edgesLength;
+            //return edgesLength; Хотел сделать функцию
         }
-        static double Perimeter(Triangle[] triangle, double[] findEdge)
+        // Метод, который формирует длину ребер и записывает их в один массив,
+        // для удобной работы в дальнейшем
+        static double[] FormedLength(Triangle triangle)
+        {
+            double[] edgeLength = new double[triangle.x.Length];
+            
+            for (int i = 0; i < triangle.x.Length; i++)
+            {
+                if (i == (triangle.x.Length - 1))
+                {
+                    edgeLength[i] = Math.Sqrt(Math.Pow(triangle.x[i - 2].x - triangle.x[i].x, 2) +
+                                              Math.Pow(triangle.x[i - 2].y - triangle.x[i].y, 2));
+                }
+                else
+                {
+                    edgeLength[i] = Math.Sqrt(Math.Pow(triangle.x[i + 1].x - triangle.x[i].x, 2) +
+                                              Math.Pow(triangle.x[i + 1].y - triangle.x[i].y, 2));
+                }
+            }
+            return edgeLength;
+        }
+        // Метод, который находит периметр каждого треугольника, здесь мы передаем длину ребер, 
+        // которую мы сформировали в предыдущем методе
+        static double Perimeter(double[] findEdge)
         {
             double perimeterTriangle = 0;
-
-                for (int i = 0; i < findEdge.Length; i++)
+                for (int j = 0; j < findEdge.Length; j++)
                 {
-                    perimeterTriangle += findEdge[i];
+                    perimeterTriangle += findEdge[j];
                 }
                 Console.WriteLine("Периметр треугольника = {0}", perimeterTriangle);
-
             return perimeterTriangle;
         }
-        static double Area(Triangle[] triangle, double[] findEdge, double perimetrtriangle)
+        // Метод, который находит площадь для кадого треугольника, здесь мы передаем длину 
+        //ребер и периметр. Зачем?(считаем по формуле полупериметра).
+        static double Area(double[] findEdge, double perimetrtriangle)
         {
             double areaTriangle = 0;
             double semiPerimeter = perimetrtriangle/2;
@@ -112,10 +154,11 @@ namespace Triangle
             Console.WriteLine("Площадь треугольника = {0}", areaTriangle);
             return areaTriangle;
         }
-        static void RightTriangle(Triangle[] triangle, double[] findEdge)
+        // Метод, который находит прямоугольный треугольник, по Пифагору
+        static void RightTriangle(double[] findEdge)
         {
             //Считаем по Пифагору
-            Array.Sort(findEdge);
+            Array.Sort(findEdge); // Грех не воспользоваться готовым методом
             if (findEdge[2] == Math.Sqrt(Math.Pow((findEdge[1]), 2) + Math.Pow((findEdge[0]), 2)))
             {
                 Console.WriteLine("Triangle is right");
@@ -125,7 +168,8 @@ namespace Triangle
                 Console.WriteLine("Triangle is't right");
             }
         }
-        static void IsoscelesTriangle(Triangle[] triangle, double[] findEdge)
+        // Метод, который находит равнобедренность каждого треугольника
+        static void IsoscelesTriangle(double[] findEdge)
         {
             if ((findEdge[0] == findEdge[1]) || (findEdge[2] == findEdge[1]) || (findEdge[2] == findEdge[0]))
             {
@@ -138,12 +182,3 @@ namespace Triangle
         }
     }
 }
-//static double[] EdgeLengthDetermination(Point[] points)
-//{
-//    double[] edgesLength = new double[3];
-//    for (int i = 0; i < edgesLength.Length; i++)
-//    {
-
-//    }
-//    return edgesLength;
-//}
